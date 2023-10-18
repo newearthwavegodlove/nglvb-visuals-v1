d8c111df7db3664952a1f2c914511f3e2f2adcbd
newearthwavegodlove <143007932+newearthwavegodlove@users.noreply.github.com>
2023-10-18 13:01:01 -0700

    ncr is here and now

---
+++ ncr.js
@@ -0,0 +1,7 @@
+function hoverEffect() {
+    document.getElementById("rainforestBtn").style.backgroundColor = "#3e8e41";
+}
+
+function normalEffect() {
+    document.getElementById("rainforestBtn").style.backgroundColor = "#4CAF50";
+}
